89641bb5d698a4888803669c863797bd779b45f0
Riya Gattellu <riya.gattellu@impressico.com>
2025-07-08 19:37:08 +0530

    Added cicd pipline

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the API!");
+  res.send("Welcome to Jooper leads!");
 });
 
 export default app;
